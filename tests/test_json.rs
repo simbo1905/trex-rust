187778ba2d124e10196830308868b7660e738f0d
simbo1905 <simbo1905@60hertz.com>
2017-07-18 21:15:36 +0100

    moving json test to the top levle

--- tests/test_json.rs
+++ tests/test_json.rs
@@ -6,7 +6,7 @@ use trex_ext::serde_trex::BallotNumberJSON;
 use trex_ext::serde_trex::JSON;
 
 #[test]
-fn test_nothing() {
+fn test_BallotNumberJSON() {
     let bn = BallotNumberJSON(BallotNumber { era: 0, counter: 1, node_identifier: 2});
 
     let json = "{\"e\":0,\"c\":1,\"n\":2}";
